eec3bdbd3ab0e9cc7c5c85e121dd135dbd5547cb
srmward <srmward@gmail.com>
2020-01-07 22:36:53 -0800

    Slight Index Work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-// code goes here
+import { Handler, Context, Callback } from 'aws-lambda';
+
+interface SentimentResponse {
+  statusCode: number;
+  body: string;
+}
+
+const getSentimentScore: Handler = (event, context: Context, cb: Callback) => {
+  const response: SentimentResponse = {
+    statusCode: 200,
+    body: "We live"
+  };
+
+  cb(null, response);
+};
+
+export { getSentimentScore }
