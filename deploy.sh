f566667e63e873b53c3fbb51f7f745b80d31d17e
srmward <srmward@gmail.com>
2020-01-13 19:40:29 -0800

    Modify Deploy Script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rm -rf dist && yarn build && cd dist/ && zip -rq sentiment.zip .
+rm -rf dist && npm run build && cd dist/ && zip -rq sentiment.zip .
 
 aws lambda update-function-code \
   --function-name getSentimentScore \
